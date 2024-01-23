c4be28d460473d20621662763cee1b5e0ea712b8
Conlincat <57761164+WheatDew@users.noreply.github.com>
2024-01-23 17:28:49 +0800

    彩虹国修改

--- Assets/OurCity/Component/CCharacterController.cs
+++ Assets/OurCity/Component/CCharacterController.cs
@@ -2,26 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class CCharacterController : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private AnimatorStateInfo currentStateInfo;
+    private float verticalInput,horizontalInput;
+
+    public Camera playerCamera;
+
+    public Vector3 verticalMultiple;
+
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+        currentStateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (verticalInput != 0 || horizontalInput != 0)
+            Move();
+
+        Animation();
     }
 
     private void Move()
     {
-        Vector3 direct = transform.forward;
-        direct.y = 0;
-        _rigidbody.velocity = direct * Input.GetAxisRaw("Horizontal");
 
+        Vector3 cameraDirect = playerCamera.transform.forward;
+
+        cameraDirect.y = 0;
+
+        transform.forward = cameraDirect;
+
+        if (currentStateInfo.IsTag("Move"))
+        {
+            if (_animator.applyRootMotion)
+                _animator.applyRootMotion = false;
+            var velocity = transform.forward.normalized * verticalInput* verticalMultiple.z+ transform.right.normalized*horizontalInput*verticalMultiple.x;
+            
+            _rigidbody.velocity = velocity;
+
+            Debug.LogFormat("{0} {1}", velocity, velocity.magnitude);
+        }
+    }
+
+    public void Animation()
+    {
+        if (currentStateInfo.IsTag("Move"))
+        {
+            _animator.SetFloat("DV", verticalInput);
+            _animator.SetFloat("DH", horizontalInput);
+        }
     }
 }
