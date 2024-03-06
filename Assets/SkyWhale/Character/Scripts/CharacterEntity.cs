bd43efce00a1e5baac505aac11b6ea6f14191223
Conlincat <57761164+WheatDew@users.noreply.github.com>
2024-03-06 17:26:15 +0800

    修改

--- Assets/SkyWhale/Character/Scripts/CharacterEntity.cs
+++ Assets/SkyWhale/Character/Scripts/CharacterEntity.cs
@@ -138,4 +138,39 @@ public class CharacterEntity : Entity
             }
         }
     }
+
+
+    //public RFX1_EffectAnimatorProperty Effect1;
+    //public GameObject Target;
+
+    //[HideInInspector] public float HUE = -1;
+    //[HideInInspector] public float Speed = -1;
+
+    //[System.Serializable]
+    //public class RFX1_EffectAnimatorProperty
+    //{
+    //    public GameObject Prefab;
+    //    public Transform BonePosition;
+    //    public Transform BoneRotation;
+    //    public float DestroyTime = 10;
+    //    [HideInInspector] public GameObject CurrentInstance;
+    //}
+
+    //public void ActivateEffect1()
+    //{
+    //    InstantiateEffect(Effect1);
+    //}
+
+    //void InstantiateEffect(RFX1_EffectAnimatorProperty effect)
+    //{
+    //    if (effect.Prefab == null) return;
+    //    effect.CurrentInstance = Instantiate(effect.Prefab, effect.BonePosition.position, effect.BoneRotation.rotation);
+
+    //    //if (Target != null)
+    //    //{
+    //    //    var target = effect.CurrentInstance.GetComponent<RFX1_Target>();
+    //    //    if (target != null) target.Target = Target;
+    //    //}
+    //    //if (effect.DestroyTime > 0.001f) Destroy(effect.CurrentInstance, effect.DestroyTime);
+    //}
 }
